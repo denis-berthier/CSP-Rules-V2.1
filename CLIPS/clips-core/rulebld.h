95df342379370e7b40f399852a1058fb38bff924
Denis Berthier <60212652+denis-berthier@users.noreply.github.com>
2021-02-11 11:54:25 +0100

    Add clips-core source files

---
+++ CLIPS/clips-core/rulebld.h
@@ -0,0 +1,65 @@
+   /*******************************************************/
+   /*      "C" Language Integrated Production System      */
+   /*                                                     */
+   /*             CLIPS Version 6.31  06/28/17            */
+   /*                                                     */
+   /*               RULE BUILD HEADER FILE                */
+   /*******************************************************/
+
+/*************************************************************/
+/* Purpose: Provides routines to ntegrates a set of pattern  */
+/*   and join tests associated with a rule into the pattern  */
+/*   and join networks. The joins are integrated into the    */
+/*   join network by routines in this module. The pattern    */
+/*   is integrated by calling the external routine           */
+/*   associated with the pattern parser that originally      */
+/*   parsed the pattern.                                     */
+/*                                                           */
+/* Principal Programmer(s):                                  */
+/*      Gary D. Riley                                        */
+/*                                                           */
+/* Contributing Programmer(s):                               */
+/*                                                           */
+/* Revision History:                                         */
+/*                                                           */
+/*      6.24: Removed INCREMENTAL_RESET compilation flag.    */
+/*                                                           */
+/*            Corrected code to remove compiler warnings.    */
+/*                                                           */
+/*      6.30: Changes to constructing join network.          */
+/*                                                           */
+/*            Added support for hashed memories.             */
+/*                                                           */
+/*      6.31: DR#882 Logical retraction not working if       */
+/*            logical CE starts with test CE.                */
+/*                                                           */
+/*************************************************************/
+
+#ifndef _H_rulebld
+
+#define _H_rulebld
+
+#ifndef _H_reorder
+#include "reorder.h"
+#endif
+#ifndef _H_network
+#include "network.h"
+#endif
+
+#ifdef LOCALE
+#undef LOCALE
+#endif
+
+#ifdef _RULEBLD_SOURCE_
+#define LOCALE
+#else
+#define LOCALE extern
+#endif
+
+   LOCALE struct joinNode               *ConstructJoins(void *,int,struct lhsParseNode *,int,struct joinNode *,int,int);
+   LOCALE void                           AttachTestCEsToPatternCEs(void *,struct lhsParseNode *);
+
+#endif /* _H_rulebld */
+
+
+
